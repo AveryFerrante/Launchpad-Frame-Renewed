f2ec92a50ecbf6f6dab58eff159f7fe91533ce05
Avery Ferrante <AFerrante@ethosgroup.com>
2019-12-24 14:37:56 -0600

    Installed Flex-Layout Created routing/entry component. Beginning to make UI

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { EntryComponent } from './entry/entry.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [EntryComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    AuthenticationRoutingModule
   ]
 })
 export class AuthenticationModule { }
