118603ffce0b7f439c0abaf5a17b21ca83633e78
Avery-Desktop <averyferrante123@gmail.com>
2020-03-03 07:58:57 -0600

    Removed Angular Material + Dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
 
 import { AngularFireModule } from '@angular/fire';
 import { FirebaseConfig } from '../environments/firebaseConfig';
@@ -22,7 +21,6 @@ import { AngularFirestore } from '@angular/fire/firestore';
     BrowserModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(FirebaseConfig),
-    MatSliderModule,
     RootStoreModule,
     AppRoutingModule
   ],
