facc74eb8a99eed47050c956f6a1a922bdad848a
Avery-Desktop <averyferrante123@gmail.com>
2020-11-25 13:03:38 -0600

    Updates to allow CI building in GitHub

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+  apiKey: 'AIzaSyAWMCReePFy4LyLLKh_DaGYnBNJPWxRoHc',
+  authDomain: 'lpf-renewed.firebaseapp.com',
+  databaseURL: 'https://lpf-renewed.firebaseio.com',
+  projectId: 'lpf-renewed',
+  storageBucket: 'lpf-renewed.appspot.com',
+  messagingSenderId: '989054372005',
+  appId: '1:989054372005:web:02ee20b634264823e7e54c',
+  measurementId: 'G-9W0SRLMCKF',
+};
